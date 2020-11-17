cfe6e52f0ffa7d7c2d4bb663e79d409d5b789e5e
jack <blueoctagon88@gmail.com>
2020-11-17 16:25:01 -0500

    Add dwarf cannon quest

---
+++ src/main/java/com/questhelper/quests/dwarfcannon/DwarfCannon.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2020, tacocats
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.questhelper.quests.YOUR_QUEST_FOLDER;
+
+@QuestDescriptor(
+        quest = QuestHelperQuest.YOUR_QUEST
+)
+public class YOURQUESTFILENAME extends BasicQuestHelper
+{
+    ItemRequirement sampleRequirement;
+    Zone sampleZone;
+    ConditionForStep sampleCondition;
+    QuestStep sampleStep;
+
+    @Override
+    public Map<Integer, QuestStep> loadSteps()
+    {
+        setupItemRequirements();
+        setupZones();
+        setupConditions();
+        setupSteps();
+
+        Map<Integer, QuestStep> steps = new HashMap<>();
+
+        return steps;
+    }
+
+    public void setupItemRequirements()
+    {
+        sampleRequirement = new ItemRequirement("Bucket", ItemID.BUCKET);
+    }
+
+    public void setupZones()
+    {
+        sampleZone = new Zone(new WorldPoint(10, 10, 0), new WorldPoint(20, 20, 0));
+    }
+
+    public void setupConditions()
+    {
+        sampleCondition = new ZoneCondition(sampleZone);
+    }
+
+    public void setupSteps()
+    {
+        sampleStep = new DetailedQuestStep(this, "Talk to Hans in Lumbridge", sampleRequirement);
+    }
+
+    @Override
+    public ArrayList<ItemRequirement> getItemRequirements()
+    {
+        return new ArrayList<>(Arrays.asList(sampleRequirement));
+    }
+
+    @Override
+    public ArrayList<ItemRequirement> getItemRecommended()
+    {
+        return new ArrayList<>(Arrays.asList());
+    }
+
+    @Override
+    public ArrayList<String> getNotes()
+    {
+        return new ArrayList<>(Arrays.asList("This is a note to appear in the sidebar"));
+    }
+
+    @Override
+    public ArrayList<String> getCombatRequirements()
+    {
+        return new ArrayList<>(Collections.singletonList("Big scary monster (level 200)"));
+    }
+
+    @Override
+    public ArrayList<PanelDetails> getPanels()
+    {
+        ArrayList<PanelDetails> allSteps = new ArrayList<>();
+        allSteps.add(new PanelDetails("The section's header!", new ArrayList<>(Arrays.asList(sampleStep)), sampleRequirement));
+        return allSteps;
+    }
+}
+
+package com.questhelper.quests.dwarfcannon;
+
+public class DwarfCannon {
+}
